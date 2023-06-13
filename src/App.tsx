9f72c6e7b2025313dc7c236b1e11a581e1f90bd9
jorgeduque007 <ivanduque-2003@hotmail.com>
2023-06-13 16:40:49 -0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box } from "@mui/material";
+import Checklist from "./components/checklist/Checklist";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box display="flex" flexDirection="column" height="100%">
+      <Box p={4} fontSize="2rem">
+        Checklist
+      </Box>
+      <Box flex="1 1" p={4}>
+        <Checklist />
+      </Box>
+    </Box>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Requireable } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import PropTypes from 'prop-types';
+import reactToWebComponent from 'react-to-webcomponent';
+import Checklist from './components/checklist/Checklist';
+import { isChecklistItemType } from './types/checklistItem';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class WebComponentWrapper extends React.Component<{ items: string }, {}> {
+  static propTypes: {
+    items: Requireable<string>;
+  };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  render() {
+    let items;
+    try {
+      items = this.props.items ? JSON.parse(this.props.items) : undefined;
+      if (!Array.isArray(items)) {
+        items = undefined;
+      } else if (items.length === 0) {
+        items = undefined;
+      } else if (items?.some(item => !isChecklistItemType(item))) {
+        items = undefined;
+      }
+    } catch {
+      items = undefined
+    }
+
+    return <Checklist items={items} />;
+  }
+}
+
+WebComponentWrapper.propTypes = {
+  items: PropTypes.string,
+}
+
+const wcChecklist = reactToWebComponent(WebComponentWrapper, React, ReactDOM, { dashStyleAttributes: true });
+const wcChecklistShadow = reactToWebComponent(WebComponentWrapper, React, ReactDOM, { dashStyleAttributes: true, shadow: true });
+
+customElements.define("r2wc-checklist", wcChecklist);
+customElements.define("r2wc-checklist-shadow", wcChecklistShadow);
+
+// How to use:
+// <script defer="defer" src="https://rjspencer.github.io/r2wc-checklist/static/js/main.js"></script> 
+// <r2wc-checklist items='[{"label":"First Thing","isChecked":false}]' />
